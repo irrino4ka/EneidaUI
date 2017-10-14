1a79ba34e0b0a2985693185de2b5c26f7cb0603e
irrino4ka <ir.rino4ka@gmail.com>
2017-10-14 14:36:24 +0300

    update api url

--- karma-unit.conf.js
+++ karma-unit.conf.js
@@ -8,6 +8,7 @@ module.exports = function(config) {
 			'https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.6.5/angular-mocks.js',
 			'app/public/script_app.js',
 			'app/public/controllers/MainController.js',
+			'app/public/controllers/EditEneidaRowController.js',
 			'app/public/services/Eneida.js',
 			'spec/unit/*.js'
 		]
